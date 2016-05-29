f4f4ada726301bdbc564e5be7805665ffabd5af8
Paddy Monahan <padmon29@gmail.com>
2016-05-29 13:55:41 +0100

    fixed app.use

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-var app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 http.listen(port, function(){
 	console.log('app listening on port '+port);
